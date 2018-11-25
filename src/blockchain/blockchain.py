b60c8ad141155ac07b5b3e3acca6d733173e67b1
Sebastian Jäger <se.jaeger@web.de>
2018-11-25 22:35:57 +0100

    First blockchain implementation with pickle

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -1,5 +1,9 @@
 import os
+import pickle
+import time
 
+from src.blockchain.block import Block
+from src.helpers.helpers import encode_file_path_properly
 
 
 class Blockchain(object):
@@ -7,21 +11,64 @@ class Blockchain(object):
     def __init__(self, path_to_chain: str) -> None:
         super().__init__()
 
+        path_to_chain = encode_file_path_properly(path_to_chain)
+
         # if local chain exists, load it
-        if os.path.exists(os.path.expanduser(path_to_chain)):
-            self.__chain = self.__load_chain(path_to_chain)
+        if os.path.exists(path_to_chain):
+            self.chain = self.__load_chain(path_to_chain)
 
         else:
-            # Create the genesis block
-            pass
+            # if no local chain exists, create the genesis block
+            genesis_block = Block(index=0, data="This is the very first Block in this chain!", proof=100, previous_hash="no previous hash, because it is the genesis block")
+            self.chain = [genesis_block]
+
+            # make sure that chain is saved to disc
+            self.__save_chain(path_to_chain)
+            self.chain = self.__load_chain(path_to_chain)
 
 
 
     def __load_chain(self, path_to_chain: str) -> list:
-        pass
+
+        #
+        if not os.path.isfile(path_to_chain):
+
+            # TODO: error handling for no existing chain
+            pass
+
+        with open(path_to_chain, mode="rb") as chain_file:
+            # TODO: handle errors: corrupt data, ...
+            chain = pickle.load(chain_file)
+
+        return chain
+
 
     def __save_chain(self, path_to_chain: str) -> None:
-        pass
+
+        if os.path.isfile(path_to_chain):
+
+            # if chain exists, rename the old one first
+            os.rename(path_to_chain, path_to_chain + "_" + time.strftime("%d-%m-%Y_%H:%M:%S", time.localtime()))
+
+        else:
+            if not os.path.isdir(os.path.dirname(path_to_chain)):
+
+                # make directories if it does not exist
+                os.makedirs(path_to_chain)
+
+        with open(path_to_chain, "wb") as chain_file:
+            pickle.dump(self.chain, chain_file)
+
 
     def new_block(self) -> None:
         pass
+
+
+    @property
+    def chain(self):
+        return self.__chain
+
+
+    @chain.setter
+    def chain(self, chain):
+        self.__chain = chain
