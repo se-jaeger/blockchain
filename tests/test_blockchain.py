91f7d9a07bf86d2608ea1b600114f5ab3ab13003
Sebastian Jäger <se.jaeger@web.de>
2018-11-25 22:26:26 +0100

    Use real chain path in test and set gitignore entry

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,5 +1,5 @@
 from src.blockchain.blockchain import *
 
 def test_constructor():
-    blockchain = Blockchain(path_to_chain="dummy_path")
+    blockchain = Blockchain(path_to_chain="test_chain/test.chain")
     assert isinstance(blockchain, Blockchain)
