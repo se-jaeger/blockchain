f8b20c7133002a577cbc74434fc33a981550247f
Sebastian Jäger <se.jaeger@web.de>
2018-11-25 16:47:59 +0100

    Create constructor and properties functions

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -1,6 +1,33 @@
+from time import time
 
 
 class Block(object):
 
-    def __init__(self) -> None:
+    def __init__(self, index: int, data: object, proof: int, previous_hash: str) -> None:
         super().__init__()
+
+        self.__index = index
+        self.__timestamp = time()
+        self.__data = data
+        self.__proof = proof
+        self.__previous_hash = previous_hash
+
+    @property
+    def index(self) -> int:
+        return self.__index
+
+    @property
+    def timestamp(self) -> float:
+        return self.__timestamp
+
+    @property
+    def data(self) -> object:
+        return self.__data
+
+    @property
+    def proof(self) -> int:
+        return self.__proof
+
+    @property
+    def previous_hash(self) -> str:
+        return self.__previous_hash
