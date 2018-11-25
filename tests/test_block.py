93c480b5fa1a095e7ea93119b884927d4075ed95
Sebastian Jäger <se.jaeger@web.de>
2018-11-25 17:19:02 +0100

    Create some tests for class: Block

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,5 +1,17 @@
 from src.blockchain.block import *
 
+
 def test_constructor():
-    block = Block()
+    block = Block(index=0, data="dummy data", proof=42, previous_hash="0815")
     assert isinstance(block, Block)
+
+    assert block.index == 0
+    assert block.data == "dummy data"
+    assert block.proof == 42
+    assert block.previous_hash == "0815"
+
+def test_timestamp():
+    block1 = Block(index=0, data="dummy data", proof=42, previous_hash="0815")
+    block2 = Block(index=0, data="dummy data", proof=42, previous_hash="0815")
+
+    assert block1.timestamp < block2.timestamp
