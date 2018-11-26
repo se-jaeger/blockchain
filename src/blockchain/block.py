cb056c9a8fff482b7530e2965869aea5c1d0d9e0
Sebastian Jäger <se.jaeger@web.de>
2018-11-26 23:04:31 +0100

    Add '__repr__' and '__eq__' functions to Block class.

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -12,6 +12,25 @@ class Block(object):
         self.__proof = proof
         self.__previous_hash = previous_hash
 
+
+    def __eq__(self, other):
+
+        # if all attributes are equal -> block1 = block2
+        return self.__dict__ == other.__dict__
+
+
+    def __repr__(self):
+
+        block_rep = "Block object with - "
+        block_rep += "index: " + str(self.index)
+        block_rep += "\tdata: " + str(self.data)
+        block_rep += "\ttime: " + str(self.timestamp)
+        block_rep += "\tproof:" + str(self.proof)
+        block_rep += "\tprevious hash:" + str(self.previous_hash)
+
+        return block_rep
+
+
     @property
     def index(self) -> int:
         return self.__index
