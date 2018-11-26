4197fec6587c78b2b532a13a3a0aa47bb777dcb2
Sebastian Jäger <se.jaeger@web.de>
2018-11-26 21:26:34 +0100

    Serialization method usable. One of [pickle, jsonpickle] are possible. Also added tests.

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -2,20 +2,22 @@ import os
 import pickle
 import time
 
+import jsonpickle
+
 from src.blockchain.block import Block
 from src.utils.utils import encode_file_path_properly
 
 
 class Blockchain(object):
 
-    def __init__(self, path_to_chain: str) -> None:
+    def __init__(self, path_to_chain: str, json_format: bool = True) -> None:
         super().__init__()
 
         path_to_chain = encode_file_path_properly(path_to_chain)
 
         # if local chain exists, load it
         if os.path.isfile(path_to_chain):
-            self.chain = self.__load_chain(path_to_chain)
+            self.chain = self.__load_chain(path_to_chain, json_format=json_format)
 
         else:
             # if no local chain exists, create the genesis block
@@ -23,45 +25,55 @@ class Blockchain(object):
             self.chain = [genesis_block]
 
             # make sure that chain is saved to disc
-            self.__save_chain(path_to_chain)
-            self.chain = self.__load_chain(path_to_chain)
+            self.__save_chain(path_to_chain, json_format=json_format)
+            self.chain = self.__load_chain(path_to_chain, json_format=json_format)
 
 
 
-    def __load_chain(self, path_to_chain: str) -> list:
+    def __load_chain(self, path_to_chain: str, json_format: bool) -> list:
 
         path_to_chain = encode_file_path_properly(path_to_chain)
 
-        #
+        # handle no existing chain
         if not os.path.isfile(path_to_chain):
 
             # TODO: error handling for no existing chain
             pass
 
-        with open(path_to_chain, mode="rb") as chain_file:
-            # TODO: handle errors: corrupt data, ...
-            chain = pickle.load(chain_file)
+        # deserialize chain from disc depending on serialization format
+        if json_format:
+            with open(path_to_chain, mode="r") as chain_file:
+                # TODO: handle errors: corrupt data, ...
+                chain = jsonpickle.decode(chain_file.read())
+        else:
+            with open(path_to_chain, mode="rb") as chain_file:
+                # TODO: handle errors: corrupt data, ...
+                chain = pickle.load(chain_file)
 
         return chain
 
 
-    def __save_chain(self, path_to_chain: str) -> None:
+    def __save_chain(self, path_to_chain: str, json_format: bool) -> None:
 
         path_to_chain = encode_file_path_properly(path_to_chain)
 
+        # if chain exists, rename the old one first
         if os.path.isfile(path_to_chain):
-
-            # if chain exists, rename the old one first
             os.rename(path_to_chain, path_to_chain + "_" + time.strftime("%d-%m-%Y_%H:%M:%S", time.localtime()))
 
         else:
-            if not os.path.isdir(os.path.dirname(path_to_chain)):
 
-                # make directories if it does not exist
+            # make directories if it does not exist
+            if not os.path.isdir(os.path.dirname(path_to_chain)):
                 os.makedirs(os.path.dirname(path_to_chain))
 
-        with open(path_to_chain, "wb") as chain_file:
-            pickle.dump(self.chain, chain_file)
+        # serialize chain to disc depending on serialization format
+        if json_format:
+            with open(path_to_chain, "w") as chain_file:
+                chain_file.write(jsonpickle.encode(self.chain))
+        else:
+            with open(path_to_chain, "wb") as chain_file:
+                pickle.dump(self.chain, chain_file)
 
 
     def new_block(self) -> None:
