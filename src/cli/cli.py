05831ecd7bd04f0b2a34c958193ec7e86c878c4f
Sebastian Jäger <se.jaeger@web.de>
2019-03-11 18:40:29 +0100

    Add basic CLI functionalities: - miner start - add <message> - get chain - get neighbours
    Change format of Logs

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -1,18 +1,32 @@
 import click
 import signal
+import logging
+import requests
+import jsonpickle
 
 from src.utils.utils import *
+from src.utils.constants import *
 from src.client.miner import Miner
 
 
+logger = logging.getLogger()
+
 
 @click.group()
-def cli():
+@click.option('--debug', default=False, is_flag=True)
+def cli(debug):
     """
     Entrypoint of CLI implementation.
     """
 
-    pass
+    # TODO reasonable Config for logging
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG, format=LOGGING_FORMAT)
+    else:
+        logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
+
+
 
 
 @cli.group()
@@ -23,15 +37,24 @@ def miner():
     pass
 
 
+@cli.group()
+def get():
+    """
+    Subcommand to get some information about the actual state.
+    """
+    pass
+
+
 @miner.command()
 @click.argument("chain_path")
 @click.option("--json", default=True, type=bool)
-@click.option("--port", default=12345, type=int)
-@click.option("--difficulty", default=5, type=int)
-@click.option("--host", default= "127.0.0.1", type=str)
-def start(chain_path: str, json: bool, host: str, port: int, difficulty: int):
+@click.option("--neighbours", default=[], type=list)
+@click.option("--port", default=PORT_DEFAULT, type=int)
+@click.option("--host", default=HOST_DEFAULT, type=str)
+@click.option("--difficulty", default=DIFFICULTY_DEFAULT, type=int)
+def start(chain_path: str, json: bool, host: str, port: int, difficulty: int, neighbours: list):
 
-    miner = Miner(path_to_chain=chain_path, json_format=json, host=host, port=port, difficulty=difficulty)
+    miner = Miner(path_to_chain=chain_path, json_format=json, host=host, port=port, difficulty=difficulty, neighbours=neighbours)
 
     try:
         signal.signal(signal.SIGTERM, signal_handler)
@@ -41,30 +64,70 @@ def start(chain_path: str, json: bool, host: str, port: int, difficulty: int):
 
     except ProgramKilledError:
 
-        # TODO cleanup...
-        print("Program killed: running cleanup code")
+        logger.error(f"Caught 'ProgramKilledError' -> Shutting down miner.")
 
-        for job in miner.jobs:
-            job.stop()
+        miner.stop_mining()
 
 
 @cli.command()
 @click.argument("message", type=str)
-@click.option("--host", default= "127.0.0.1", type=str)
-@click.option("--port", default=12345, type=int)
+@click.option("--port", default=PORT_DEFAULT, type=int)
+@click.option("--host", default=HOST_DEFAULT, type=str)
 def add(message, host, port):
     """
     Send a message to known miners.
     """
 
-    # TODO: send given message  to local miner ...
+    response = requests.put(create_URL(host, port, ADD_ENDPOINT), params={MESSAGE_PARAM: message})
 
-    pass
+    if response.status_code == HTTP_OK:
+
+        json = response.json()
+
+        click.echo(click.style(json["message"], fg="green"))
+
+    else:
+        click.echo(click.style("Something went wrong!", fg="red"))
 
 
-@cli.command()
-def asdf():
-    miner = Miner("/Users/sjaeger/Desktop/asdf/test.chain", neighbours=[("localhost", 12345)], json_format=True, host="127.0.0.1", port=12345, difficulty=5)
+@get.command()
+@click.option("--port", default=PORT_DEFAULT, type=int)
+@click.option("--host", default=HOST_DEFAULT, type=str)
+def chain(host, port):
+    """
+    Get the actual chain.
+    """
 
-    print(miner.check_for_longest_chain())
-    print(miner.blockchain.chain)
+    response = requests.get(create_URL(host, port, CHAIN_ENDPOINT))
+
+    if response.status_code == HTTP_OK:
+
+        json = response.json()
+        chain = jsonpickle.decode(json["chain"])
+
+        click.echo(click.style(f"\nChain with length: {json['length']}\n", fg="green"))
+        click.echo(chain)
+
+    else:
+        click.echo(click.style("Something went wrong!", fg="red"))
+
+
+@get.command()
+@click.option("--port", default=PORT_DEFAULT, type=int)
+@click.option("--host", default=HOST_DEFAULT, type=str)
+def neighbours(host, port):
+    """
+    Get the actual neighbours
+    """
+
+    response = requests.get(create_URL(host, port, SEND_NEIGHBOURS_KEY))
+
+    if response.status_code == HTTP_OK:
+
+        json = response.json()
+
+        click.echo(click.style(f"\nActual are {json['length']} neighbours available.\n", fg="green"))
+        click.echo(json["neighbours"])
+
+    else:
+        click.echo(click.style("Something went wrong!", fg="red"))
