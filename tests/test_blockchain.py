6e1309c025e455cc9d1e9c848b01aa89df48dcc4
Sebastian Jäger <se.jaeger@web.de>
2019-03-14 11:10:15 +0100

    Fix tests. - Add 'force_new_chain' argument to constructors

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -28,13 +28,13 @@ def clean_chain_file_fixture():
 def test_constructor(json_format, clean_chain_file_fixture):
 
     # test without local chain
-    blockchain_without_local_chain = Blockchain(path_to_chain=path_to_chain, json_format=json_format)
+    blockchain_without_local_chain = Blockchain(path_to_chain=path_to_chain, json_format=json_format, force_new_chain=False)
     assert isinstance(blockchain_without_local_chain, Blockchain)
     assert blockchain_without_local_chain.chain[0] == GENESIS_BLOCK
     assert blockchain_without_local_chain.last_block == GENESIS_BLOCK
 
     # test with local chain
-    blockchain_with_local_chain = Blockchain(path_to_chain=path_to_chain, json_format=json_format)
+    blockchain_with_local_chain = Blockchain(path_to_chain=path_to_chain, json_format=json_format, force_new_chain=False)
     assert isinstance(blockchain_with_local_chain, Blockchain)
     assert blockchain_with_local_chain.chain[0] == GENESIS_BLOCK
     assert blockchain_with_local_chain.last_block == GENESIS_BLOCK
@@ -42,7 +42,7 @@ def test_constructor(json_format, clean_chain_file_fixture):
 
 @pytest.mark.parametrize("json_format", constructor_json_format)
 def test_add_new_block(json_format, clean_chain_file_fixture):
-    blockchain = Blockchain(path_to_chain=path_to_chain, json_format=json_format)
+    blockchain = Blockchain(path_to_chain=path_to_chain, json_format=json_format, force_new_chain=False)
 
     blockchain.add_new_block(data=Data("second block"), proof=4711, previous_hash="e59be601c9213694f0b72534148199b24d1ed7c1c29c02ba4602e780015a7663")
     blockchain.add_new_block(data=Data("third block"), proof=42, previous_hash="637ae601c9213694f0b72534148199b24d1ed7c1c29c02ba4602e780015a09ab")
@@ -63,7 +63,7 @@ def test_add_new_block(json_format, clean_chain_file_fixture):
 @pytest.mark.parametrize("json_format", constructor_json_format)
 def test_chain_method(json_format, clean_chain_file_fixture):
 
-    blockchain = Blockchain(path_to_chain=path_to_chain, json_format=json_format)
+    blockchain = Blockchain(path_to_chain=path_to_chain, json_format=json_format, force_new_chain=False)
     chain = blockchain.chain
     genesis_block = chain[0]
 
